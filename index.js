2863510316df741a80a1f793300690fb05dc8712
Borbala Mester <mest0006@alognquinlive.com>
2020-06-20 12:35:38 -0400

    changes made

--- index.js
+++ index.js
@@ -38,38 +38,6 @@ div.innerHTML = products.join('')
 
 
 
-function myFunction () {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
-
-window.onclick = function (event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
-function myFunction1 () {
-  document.getElementById("myDropdown1").classList.toggle("show");
-}
-
-window.onclick = function (event) {
-  if (!event.target.matches('.dropbtn1')) {
-    var dropdowns1 = document.getElementsByClassName("dropdown-content1");
-    var i;
-    for (i = 0; i < dropdowns1.length; i++) {
-      var openDropdown1 = dropdowns1[i];
-      if (openDropdown1.classList.contains('show')) {
-        openDropdown1.classList.remove('show');
-      }
-    }
-  }
-}
 
 
 
@@ -78,16 +46,15 @@ window.onclick = function (event) {
 
 
 
-// create references to the modal...
 var modal = document.getElementById('myModal');
-// to all images -- note I'm using a class!
+
 var images = document.getElementsByClassName('image');
-// the image in the modal
+
 var modalImg = document.getElementById("img01");
 
 
 
-// Go through all of the images with our custom class
+
 for (var i = 0; i < products.length; i++) {
   var img = images[i];
 
@@ -121,5 +88,285 @@ for (var i = 0; i < img.length; i++) {
   modalImg.textContent = "`${products[i].description}`";
 }
 
+function itemfilter () {
+  const fil = document.getElementById('select-category2').value
+
+  if (fil === 'gold') {
+    console.log('yes, gold')
+
+    const filteredpro = product.filter((g) => g.description.includes('gold'))
+    const filterProd = []
+    for (const pro of filteredpro) {
+      var div = document.getElementById('items')
+      filterProd.push(
+        (div.innerHTML = ` 
+  
+    <div class="div">
+    <div category="all" class="product">
+      <img class="image" src="${pro.image_link_mb}" alt="" width="500px"
+        style="border-bottom: 1px solid light-grey">
+      <div class="des">
+        <p class="title"> ${pro.title}</p>
+      </div>
+      <p data-price="${pro.price}">
+        <strike>${pro.crossed_price}</strike><strong>${pro.price}</strong>
+    </div>
+    <div id="myModal" class="modal">
+      <div id="img01"> ${pro.description} <button class="buynow"> BUY NOW</button>
+        <span class="close">&times;</span> </div>
+      </p>
+  
+    </div>
+  
+  </div>
+  `)
+      )
+    }
+    div.innerHTML = filterProd.join('')
+    document.getElementById('result').innerHTML = filterProd.length
+  } else if (fil === 'silver') {
+    console.log('yes, silver')
+
+    const filteredpro = product.filter((g) => g.description.includes('silver'))
+    const filterProd = []
+    for (const pro of filteredpro) {
+      var div = document.getElementById('items')
+      filterProd.push(
+        (div.innerHTML = ` 
+  
+    <div class="div">
+    <div category="all" class="product">
+      <img class="image" src="${pro.image_link_mb}" alt="" width="500px"
+        style="border-bottom: 1px solid light-grey">
+      <div class="des">
+        <p class="title"> ${pro.title}</p>
+      </div>
+      <p data-price="${pro.price}">
+        <strike>${pro.crossed_price}</strike><strong>${pro.price}</strong>
+    </div>
+    <div id="myModal" class="modal">
+      <div id="img01"> ${pro.description} <button class="buynow"> BUY NOW</button>
+        <span id="close" class="close">&times;</span> </div>
+      </p>
+  
+    </div>
+  
+  </div>
+  `)
+      )
+    }
+    div.innerHTML = filterProd.join('')
+    document.getElementById('result').innerHTML = filterProd.length
+  }
 
 
+  var modal = document.getElementById('myModal');
+
+  var images = document.getElementsByClassName('image');
+
+  var modalImg = document.getElementById("img01");
+
+
+
+
+  for (var i = 0; i < products.length; i++) {
+    var img = images[i];
+
+    img.onclick = function (evt) {
+      modal.style.display = "block";
+      modalImg.src = this.src;
+
+
+    }
+  }
+
+  var span = document.getElementById("close")[0];
+
+  span.onclick = function () {
+    modal.style.display = "none";
+  }
+
+
+
+
+
+  var showModal = function () {
+    modal.style.display = "block";
+    modalImg.textContent = `${products[i].description}`;
+
+  }
+
+
+  for (var i = 0; i < img.length; i++) {
+    img[i].addEventListener('click', showModal);
+    modalImg.textContent = "`${products[i].description}`";
+  }
+}
+
+
+
+function itemSort () {
+  const sor = document.getElementById('select-category').value
+  if (sor === 'highest') {
+    const sorted = product.sort((b, a) =>
+      a.price < b.price ? -1 : a.price > b.price ? 1 : 0
+    )
+    var sortedProd = []
+    for (const sort of product) {
+      sortedProd.push(`<div class="div">
+    <div category="all" class="product">
+      <img class="image" src="${sort.image_link_mb}" alt="" width="500px"
+        style="border-bottom: 1px solid light-grey">
+      <div class="des">
+        <p class="title"> ${sort.title}</p>
+      </div>
+      <p data-price="${sort.price}">
+        <strike>${sort.crossed_price}</strike><strong>${sort.price}</strong>
+    </div>
+    <div id="myModal" class="modal">
+      <div id="img01"> ${sort.description} <button class="buynow"> BUY NOW</button>
+        <span class="close">&times;</span> </div>
+      </p>
+  
+    </div>
+  
+  </div>`)
+    }
+    div.innerHTML = sortedProd.join('')
+    document.getElementById('result').innerHTML = sortedProd.length
+  } else if (sor === 'lowest') {
+    const sorted = product.sort((a, b) =>
+      a.price < b.price ? -1 : a.price > b.price ? 1 : 0
+    )
+    var sortedProd = []
+    for (const sort of product) {
+      sortedProd.push(`<div class="div">
+    <div category="all" class="product">
+      <img class="image" src="${sort.image_link_mb}" alt="" width="500px"
+        style="border-bottom: 1px solid light-grey">
+      <div class="des">
+        <p class="title"> ${sort.title}</p>
+      </div>
+      <p data-price="${sort.price}">
+        <strike>${sort.crossed_price}</strike><strong>${sort.price}</strong>
+        <div id="myModal" class="modal">
+        <div id="img01"> ${sort.description} <button class="buynow"> BUY NOW</button>
+          <span class="close">&times;</span> </div>
+        </p>
+    
+      </div>
+    </div>
+
+    
+ 
+  
+  </div>`)
+    }
+    div.innerHTML = sortedProd.join('')
+    document.getElementById('result').innerHTML = sortedProd.length
+  }
+
+  // create references to the modal...
+  var modal = document.getElementById('myModal');
+  // to all images -- note I'm using a class!
+  var images = document.getElementsByClassName('image');
+  // the image in the modal
+  var modalImg = document.getElementById("img01");
+
+
+
+  // Go through all of the images with our custom class
+  for (var i = 0; i < products.length; i++) {
+    var img = images[i];
+
+    img.onclick = function (evt) {
+      modal.style.display = "block";
+      modalImg.src = this.src;
+
+
+    }
+  }
+
+  var span = document.getElementsByClassName("close")[0];
+
+  span.onclick = function () {
+    modal.style.display = "none";
+  }
+
+
+
+
+
+  var showModal = function () {
+    modal.style.display = "block";
+    modalImg.textContent = `${products[i].description}`;
+
+  }
+
+
+  for (var i = 0; i < img.length; i++) {
+    img[i].addEventListener('click', showModal);
+    modalImg.textContent = "`${products[i].description}`";
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var modal = document.getElementById('myModal')
+var images = document.getElementsByClassName('image');
+var modalImg = document.getElementById("img01");
+
+
+
+
+for (var i = 0; i < products.length; i++) {
+  var img = images[i];
+
+  img.onclick = function (evt) {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+
+
+  }
+}
+
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function () {
+  modal.style.display = "none";
+}
+
+
+
+
+
+var showModal = function () {
+  modal.style.display = "block";
+  modalImg.textContent = `${products[i].description}`;
+
+}
+
+
+for (var i = 0; i < img.length; i++) {
+  img[i].addEventListener('click', showModal);
+  modalImg.textContent = "`${products[i].description}`";
+}
